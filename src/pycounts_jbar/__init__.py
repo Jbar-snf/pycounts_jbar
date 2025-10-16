ab546e08666cfbd2b8ed7df6dcb0193eb26a9159
U1094323 <jeremy.barbier@sanofi.com>
2025-10-16 14:52:37 +0000

    feat: add count_words and plot_words to package namespace

--- src/pycounts_jbar/__init__.py
+++ src/pycounts_jbar/__init__.py
@@ -1,3 +1,6 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_jbar")
+
+from pycounts_jbar.pycounts_jbar import count_words
+from pycounts_jbar.plotting import plot_words
